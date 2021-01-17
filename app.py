1c4c24b1841ee021d9edf3bca5718467677d7da5
Anton Högelin <anthge-7@student.ltu.se>
2021-01-17 13:05:09 +0100

    Created an AI Dojo to let it train during running games. training.py takes care of the reinforcement learning.

--- app.py
+++ app.py
@@ -4,39 +4,74 @@ from rps_ml.baseline.rpscontest_agent import DLLU1Agent, MetaFixAgent
 from rps_ml.ai_agent.ai_agent import AiAgent
 from rps_ml.ai_agent.models import DefaultGameplayModel, DefaultPredictionModel
 
+from rps_ml.ai_agent.training import RPSDojo
 
-
+import tensorflow as tf
+import csv
 
 NUM_ROUNDS = 1000
-NUM_GAMES = 100
+NUM_GAMES = 5
+MODEL_LOCATION = 'SavedModels/BestGameplayModel'
+DATA_LOCATION = 'Statistics/'
 
 p1 = MetaFixAgent()
 p2 = RepeatAgent()
 
-
 gameplay_model = DefaultGameplayModel()
 prediction_model = DefaultPredictionModel()
 
-gameplay_model.compile(optimizer='rmsprop', loss='mse')
+gameplay_model.compile(loss='mse')
 prediction_model.compile(optimizer=None, loss='categorical_crossentropy')
 
-pai = AiAgent(prediction_model=prediction_model,
-              gameplay_model=gameplay_model)
+
+def run_training():
+    trainee = AiAgent(prediction_model, gameplay_model)
+    trainer = MetaFixAgent()
+
+    dojo = RPSDojo(trainee=trainee,
+                   trainer=trainer,
+                   episodes=NUM_GAMES,
+                   optimizer=tf.keras.optimizers.Adam(),
+                   loss=tf.keras.losses.MeanSquaredError(),
+                   discount=0.95)
+
+    dojo.run_training()
+
+    gameplay_model.save(MODEL_LOCATION, include_optimizer=False)
+
+    with open(DATA_LOCATION + 'training_history.csv', 'w', newline='') as f:
+        w = csv.writer(f, dialect='excel')
+        w.writerows(dojo.training_history)
+
+    with open(DATA_LOCATION + 'action_history.csv', 'w', newline='') as f:
+        w = csv.writer(f, dialect='excel')
+        w.writerows(trainee.history)
 
 
-p1 = pai
-p2 = MetaFixAgent()
 
-game = GameRunner(p1, p2, num_rounds=NUM_ROUNDS)
 
-score = {p1: 0, p2: 0}
-for i in range(NUM_GAMES):
-    print("\rPlaying game %d / %d" % (i+1, NUM_GAMES), end="")
-    while not game.game_over:
-        game.play_round()
-    winner = game.winner
-    if winner is not None:
-        score[winner] += 1
-    game.reset()
-print()
-print(score)
+
+
+
+def test_simple_match():
+    pai = AiAgent(prediction_model=prediction_model,
+                  gameplay_model=gameplay_model)
+
+    p1 = pai
+    p2 = MetaFixAgent()
+
+    game = GameRunner(p1, p2, num_rounds=NUM_ROUNDS)
+
+    score = {p1: 0, p2: 0}
+    for i in range(NUM_GAMES):
+        print("\rPlaying game %d / %d" % (i + 1, NUM_GAMES), end="")
+        while not game.game_over:
+            game.play_round()
+        winner = game.winner
+        if winner is not None:
+            score[winner] += 1
+        game.reset()
+    print()
+    print(score)
+
+run_training()
---
+++ rps_ml/ai_agent/training.py
@@ -0,0 +1,128 @@
+import tensorflow as tf
+
+from rps.agent.base_agent import RPSAgent
+from rps_ml.ai_agent.ai_agent import AiAgent
+from rps.constants import GameState, Move
+
+from rps.game.GameRunner import GameRunner
+import rps.utils.utils as utils
+import numpy as np
+
+
+class RPSDojo():
+
+    def __init__(self, trainee: AiAgent, trainer: RPSAgent, episodes: int,
+                 optimizer: tf.keras.optimizers.Optimizer, loss: tf.keras.losses.Loss,
+                 batch_size=64, discount=0.9, rounds_in_episode=1000) -> None:
+        """Instantiates a new training setup for the ML Agent
+
+        @:param trainee The AI Agent to be trained
+        @:param trainer The RPSAgent which the AI Agent should train against
+        @:param episodes The number of episodes to run
+        @:param optimizer A Keras optimizer for use with training
+        @:param loss A Keras Loss funmodection for use with training
+        @:param batch_size The batch size to use for training the model
+        @:param discount The discount to apply for older actions
+        @:param rewards A Set containing the various GameStates to compute a reward for the agent."""
+
+        assert isinstance(episodes, int), "Episodes must be a given number."
+        assert episodes > 0, "Episodes must be greater than 0"
+
+        self.trainee = trainee
+        self.trainer = trainer
+
+        self.episodes = episodes
+        self.optimizer = optimizer
+        self.loss = loss
+        self.discount = discount
+        self.batch_size = batch_size
+        self.game = GameRunner(player_one=trainee,
+                               player_two=trainer,
+                               num_rounds=rounds_in_episode)
+
+        self.best_reward = 0
+        self.best_weights = None
+
+        self.training_history = []
+
+    def run_training(self):
+        model = self.trainee.gameplay_model
+        loss = -1
+        for i in range(self.episodes):
+
+            game_history = []
+            total_reward = 0
+            round_count = 0
+
+            while not self.game.game_over:
+                round_count += 1
+                trainee_move, trainer_move, winner, game_over = self.game.play_round()
+
+                trainee_state = utils.check_winner(trainee_move, trainer_move)
+                reward = self.trainee.rewards[trainee_state]
+
+                game_history.append((trainee_move, trainer_move, reward))
+                total_reward += reward
+
+                if round_count % 20 == 0:
+                    print("\rTraining game: %d / %d (%d pct) - Total reward: %d" %
+                          (i + 1, self.episodes, (round_count / self.game.num_rounds) * 100, total_reward),
+                          end="")
+
+            loss = self.training_step()
+
+            self.training_history.append((loss, total_reward))
+
+            # total_reward = tf.reduce_sum(self.trainee.experiences, axis=4)
+
+            if self.best_weights is None or total_reward > self.best_reward:
+                self.best_reward = total_reward
+                self.best_weights = model.get_weights()
+
+            print("\rTraining game: %d / %d (%d pct) - Total reward: %d , Loss: %d" %
+                  (i + 1, self.episodes, (round_count / self.game.num_rounds) * 100, total_reward, loss),
+                  end="")
+
+            self.game.reset()
+            print()
+
+        model.set_weights(self.best_weights)
+
+
+
+    def training_step(self, batch_size=32):
+        model = self.trainee.gameplay_model
+        experiences = self.sample_experiences()
+
+        states, new_states, actions, rewards = experiences
+
+        next_q = model.predict(new_states)
+        max_next_values = np.max(next_q, axis=2)
+        rewards = rewards.reshape(rewards.shape + (1,))
+        target_q = (rewards + self.discount * max_next_values)
+        # target_q = target_q.reshape(-1, 1)
+
+        # Offset actions by - 1 since the value of it is based on the Move enum
+        mask = tf.one_hot(actions - 1, len(Move))
+
+        with tf.GradientTape() as tape:
+            all_q = model(states)
+            q_values = tf.reduce_sum(all_q * mask, axis=1, keepdims=True)
+            loss = tf.reduce_mean(self.loss(target_q, q_values))
+
+        grads = tape.gradient(loss, model.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+        return loss
+
+    # Gathered from
+    # https://colab.research.google.com/github/ageron/handson-ml2/blob/master/18_reinforcement_learning.ipynb#scrollTo=K912ogLfbdYX
+    def sample_experiences(self, batch_size=32):
+        experiences = self.trainee.experiences
+
+        indices = np.random.randint(len(experiences), size=batch_size)
+        batch = [experiences[index] for index in indices]
+        states, new_states, actions, rewards = [
+            np.array([experience[field_index] for experience in batch])
+            for field_index in range(4)]
+        return states, new_states, actions, rewards
